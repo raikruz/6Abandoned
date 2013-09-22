5d59056322c4ac80eccd89e7b5d6cefc2bed6733
SealedOrpheus <gold_fenrir007@yahoo.com>
2013-09-23 02:04:14 +0800

    Putting Bee Scripts and prefab

---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/BeeAnimation.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+var bumpMap : Texture;
+var bumpMap2 : Texture;
+var bumpMap3 : Texture;
+var image : int;
+function Start () {
+}
+
+function Update () {
+	if(image==0)
+	{
+		renderer.material.SetTexture("_MainTex", bumpMap);
+		UpdateImageDelay();
+	}
+	else if(image==1)
+	{	
+		renderer.material.SetTexture("_MainTex", bumpMap2);
+		UpdateImageDelay();
+	}else if(image==2)
+	{	
+		renderer.material.SetTexture("_MainTex", bumpMap3);
+		UpdateImageDelay();
+	}
+
+}
+
+function UpdateImageDelay () {
+	yield WaitForSeconds(2);
+	if(image==0)
+	{
+		image=1;
+	}
+	else if(image==1)
+	{
+		image=2;
+	}else if(image==2)
+	{
+		image=0;
+	}
+}
---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/BeeMovement.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var direction : int ;
+
+function Start () {
+
+}
+
+function Update() {
+		// Move the object to the right relative to the camera 1 unit/second.
+		if(direction==0)
+		{
+			transform.Translate(Vector3.right * (Time.deltaTime*0.1), Camera.main.transform);
+		}
+		if(direction==1)
+		{
+			transform.Translate(Vector3.left * (Time.deltaTime*0.1), Camera.main.transform);
+		}
+}
