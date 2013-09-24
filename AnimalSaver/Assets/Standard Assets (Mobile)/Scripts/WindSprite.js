85a8fadf819e9ad614eed2333f2ad276112c92c5
fatrat1117 <fatrat1117@gmail.com>
2013-09-24 16:54:29 +0800

    Update WindSprite Prefab

---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/WindSprite.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var direction = "Down";
+
+function Start () {
+
+}
+
+function Update () {
+	if (direction == "down") {
+	}
+	else if (direction == "left") {
+	//update image and resolution 
+	}
+	else if (direction == "right") {
+	}
+}
