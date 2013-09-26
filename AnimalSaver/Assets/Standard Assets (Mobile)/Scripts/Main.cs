f6e5f0ce408d3ee6f2282725b593d7d9026e4562
FrancisLuo2891 <luo_xiawu@163.com>
2013-09-27 00:51:05 +0800

    Make the Cow & Chick come out slowly.
    1. Add Left & Right pictures to respond Left & Right Keys;
    2. Need more test.

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Main.cs
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Main.cs
@@ -6,12 +6,30 @@ public class Main : MonoBehaviour {
 	bool initialized = false;           // initialization notifier
     bool scrolling = false;             // scrolling notifier
 	
+	// Cow picture;
+	Texture cow_left = null;
+	Texture cow_right = null;
+	Texture cow_down = null;
+	// Chick picture
+	Texture chick_left = null;
+	Texture chick_right = null;
+	Texture chick_down = null;
+	
 	// Use this for initialization
 	void Start () {
 		// resize filled sprites to match screen size
         Resize("BackGround");
         // set initialized notifier to true so we only initialize once.
         initialized = true;
+		
+		// Cow picture;
+		cow_left = Resources.Load("cow_left") as Texture;
+		cow_right = Resources.Load("cow_right") as Texture;
+		cow_down = Resources.Load("cow_down") as Texture;
+		// Chick picture
+		chick_left = Resources.Load("chick_left") as Texture;
+		chick_right = Resources.Load("chick_right") as Texture;
+		chick_down = Resources.Load("chick_down") as Texture;
 	}
 	
 
@@ -33,14 +51,53 @@ public class Main : MonoBehaviour {
 	System.Random random = new System.Random();
 	// Update is called once per frame
 	float t;
+	int index = 0;
+	Texture animal_texture = null;
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		int randomNumber = random.Next(0, 2);
+		
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+	    {
+			if(randomNumber == 0)
+			{
+				animal_texture = cow_left;
+			}
+			else
+			{
+				animal_texture = chick_left;
+			}
+	    }
+	    else if (Input.GetKeyDown(KeyCode.RightArrow))
+	    {
+			if(randomNumber == 0)
+			{
+				animal_texture = cow_right;
+			}
+			else
+			{
+				animal_texture = chick_right;
+			}
+	    }
+		else
+		{
+			if(randomNumber == 0)
+			{
+				animal_texture = cow_left;
+			}
+			else
+			{
+				animal_texture = chick_down;
+			}
+		}
 		
 		t += Time.deltaTime;
-		if (t > 0.5f)
+		if (t > 1.2f)
 		{
-			int randomNumber = random.Next(0, 1);
+			
 			GameObject obj = OT.CreateObject(animals[randomNumber]);
+			obj.guiTexture.texture = animal_texture;
 			t = 0;
 		}
 		
