cb1c57efd61ebe2a1546a177ce48fe773fc5c156
fatrat1117 <fatrat1117@gmail.com>
2013-09-26 16:25:04 +0800

    Main.cs

---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Main.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	
+	bool initialized = false;           // initialization notifier
+    bool scrolling = false;             // scrolling notifier
+	
+	// Use this for initialization
+	void Start () {
+		// resize filled sprites to match screen size
+        Resize("BackGround");
+        // set initialized notifier to true so we only initialize once.
+        initialized = true;
+	}
+	
+
+    // This method will resize the a FilledSprite ( provided by name )
+    // to match the current view (resolution).
+    void Resize(string spriteName)
+    {
+        // Lookup the FilledSprite using its name.
+        OTObject sprite = OT.ObjectByName(spriteName);
+        if (sprite != null)
+        {
+            // We found the sprite so lets size it to match the screen's resolution
+            // We will assume the OTView.zoom factor is set to zero (no zooming)
+            sprite.size = new Vector2(Screen.width, Screen.height);
+        }
+    }
+	
+	static string[] animals = new string[] {"Cow_pre", "Chick_pre"};
+	System.Random random = new System.Random();
+	// Update is called once per frame
+	float t;
+	// Update is called once per frame
+	void Update () {
+		
+		t += Time.deltaTime;
+		if (t > 0.5f)
+		{
+			int randomNumber = random.Next(0, 1);
+			GameObject obj = OT.CreateObject(animals[randomNumber]);
+			t = 0;
+		}
+		
+	}
+}
