ad133132118f831423a8d08fbb5aa26d326a2ea6
FrancisLuo2891 <luo_xiawu@163.com>
2013-09-27 23:12:51 +0800

    Implement the function 'Left & Right Movement of Prefab'
    Cow & Chick can move to left & right respond to the the left Arrow &
    right Arrow of the key.

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
@@ -1,75 +1,34 @@
 ﻿#pragma strict
-
-//public var bullet:Transform;
-
-public var speed:float = 20.0f;
-public var speedy:float = 1.0f;
-public var direction:int = 1;
-public var startingLives:int = 3;
-
-public var abc: Transform;
-private var startPos: Vector3;
-private var start_x:float = 0.5;
-private var start_y:float = 0.87;
-private var start_z:float = 0;
 private var Cow_right : Texture;
 private var Cow_left : Texture;
 private var Cow_down : Texture;
 
-private var remainingLives: int;
-
-function Start () {
-	remainingLives = startingLives;
-	// initialize variables
-	startPos.x = start_x;
-	startPos.y = start_y;
-	startPos.z = start_z;
-	//
+function Start () 
+{
+	// initialize picture
 	Cow_right = Resources.Load("cow_right") as Texture;
     Cow_left = Resources.Load("cow_left") as Texture;
     Cow_down = Resources.Load("cow_down") as Texture;
-    //Cow_down = Resources.Load("Standard Assets (Mobile)/Textures/cow_down") as Texture;
 }
 
-function Update () {
-	var targetPos: Vector3 = transform.position;
-	if (Input.GetKey(UnityEngine.KeyCode.LeftArrow)) {
-	    // change the cow's picture to the left one
-	    GameObject.Find("Cow_pre").guiTexture.texture = Cow_left;
-		targetPos.x = transform.position.x - 0.02;
-    	transform.position = Vector3.Lerp (transform.position, targetPos, Time.deltaTime * 1.0f);  	
+function Update () 
+{
+	// respond to the left arrow
+	if(Input.GetKey(KeyCode.LeftArrow))
+	{
+		gameObject.guiTexture.texture = Cow_left;
+		transform.Translate(-0.2 * Time.deltaTime, 0, 0);
 	}
-	else if (Input.GetKey(UnityEngine.KeyCode.RightArrow)) {
-	    // change the cow's picture to the right one
-	    GameObject.Find("Cow_pre").guiTexture.texture = Cow_right;
-		targetPos.x = transform.position.x + 0.02;
-    	transform.position = Vector3.Lerp (transform.position, targetPos, Time.deltaTime * 1.0f);
-    }
-    else
-    {
-    	GameObject.Find("Cow_pre").guiTexture.texture = Cow_down;
-    }
-		
-    if (Input.GetKeyDown("space"))
-    {
-    	// need to implement sth...
-    }
-    targetPos.y = transform.position.y - speedy * direction/30;
-    transform.position = Vector3.Lerp (transform.position, targetPos, Time.deltaTime * 1.0f);
+	// respond to the right arrow
+	else if(Input.GetKey(KeyCode.RightArrow))
+	{
+		gameObject.guiTexture.texture = Cow_right;
+		transform.Translate(0.2 * Time.deltaTime, 0, 0);
+	}
+	// default down picture
+	else
+	{
+		gameObject.guiTexture.texture = Cow_down;
+	}
     
-}
-
-function OnGUI() {
-    //GUI.Label (Rect (70, 5, 120, 20), "Lives Remaining: " + remainingLives);
-}
-
-function Hit() {
-	remainingLives--;
-	
-	//Instantiate(explosion, transform.position, Quaternion.identity);
-	
-	if (remainingLives < 0) {
-		Destroy(gameObject);
-		GameObject.Find("GameScript").SendMessage("PlayerDead");
-	}
 }
--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Main.cs
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Main.cs
@@ -6,30 +6,12 @@ public class Main : MonoBehaviour {
 	//bool initialized = false;           // initialization notifier
     bool scrolling = false;             // scrolling notifier
 	
-	// Cow picture;
-	Texture cow_left = null;
-	Texture cow_right = null;
-	Texture cow_down = null;
-	// Chick picture
-	Texture chick_left = null;
-	Texture chick_right = null;
-	Texture chick_down = null;
-	
 	// Use this for initialization
 	void Start () {
 		// resize filled sprites to match screen size
         //Resize("BackGround");
         // set initialized notifier to true so we only initialize once.
         //initialized = true;
-		
-		// Cow picture;
-		cow_left = Resources.Load("cow_left") as Texture;
-		cow_right = Resources.Load("cow_right") as Texture;
-		cow_down = Resources.Load("cow_down") as Texture;
-		// Chick picture
-		chick_left = Resources.Load("chick_left") as Texture;
-		chick_right = Resources.Load("chick_right") as Texture;
-		chick_down = Resources.Load("chick_down") as Texture;
 	}
 	
 
@@ -52,55 +34,17 @@ public class Main : MonoBehaviour {
 	System.Random random = new System.Random();
 	// Update is called once per frame
 	float t;
-	int index = 0;
-	Texture animal_texture = null;
 	// Update is called once per frame
 	void Update () 
 	{
 		int randomNumber = random.Next(0, 2);
-		
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-	    {
-			if(randomNumber == 0)
-			{
-				animal_texture = cow_left;
-			}
-			else
-			{
-				animal_texture = chick_left;
-			}
-	    }
-	    else if (Input.GetKeyDown(KeyCode.RightArrow))
-	    {
-			if(randomNumber == 0)
-			{
-				animal_texture = cow_right;
-			}
-			else
-			{
-				animal_texture = chick_right;
-			}
-	    }
-		else
-		{
-			if(randomNumber == 0)
-			{
-				animal_texture = cow_left;
-			}
-			else
-			{
-				animal_texture = chick_down;
-			}
-		}
 		
 		t += Time.deltaTime;
 		if (t > 1.2f)
 		{
 			
 			GameObject obj = OT.CreateObject(animals[randomNumber]);
-			obj.guiTexture.texture = animal_texture;
-			//OTSprite boat = OT.CreateObject("Boat").GetComponent<OTSprite>();
-			//boat.position = new Vector2(50, 100);
+			//obj.guiTexture.texture = animal_texture;
 			t = 0;
 		}
 	}
