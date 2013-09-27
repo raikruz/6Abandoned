6a5fded02a8bb7f2385b33c43ffd598fd59a072d
FrancisLuo2891 <luo_xiawu@163.com>
2013-09-27 23:45:26 +0800

    Add the function "Cow & Chick die when hitting both sides of the background"
    When they hit left or right border of the background picture, they will
    die and fall down.

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Chick.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Chick.js
@@ -2,6 +2,8 @@
 private var Chick_right : Texture;
 private var Chick_left : Texture;
 private var Chick_down : Texture;
+private var Chick_dead : Texture;
+private var Chick_die = false;
 
 function Start()
 {
@@ -9,24 +11,35 @@ function Start()
 	Chick_right = Resources.Load("chick_right") as Texture;
     Chick_left = Resources.Load("chick_left") as Texture;
     Chick_down = Resources.Load("chick_down") as Texture;
+    Chick_dead = Resources.Load("chick_dead") as Texture;
 }
 
 function Update () {
 	// respond to the left arrow
-	if(Input.GetKey(KeyCode.LeftArrow))
+	if(Input.GetKey(KeyCode.LeftArrow) && Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_left;
 		transform.Translate(-0.3 * Time.deltaTime, 0, 0);
 	}
 	// respond to the right arrow
-	else if(Input.GetKey(KeyCode.RightArrow))
+	else if(Input.GetKey(KeyCode.RightArrow) && Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_right;
 		transform.Translate(0.3 * Time.deltaTime, 0, 0);
 	}
 	// default down picture
-	else
+	else if(Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_down;
+	}
+	
+	// determine the border of the object's movement
+	// the middle x position is: x = 0.5
+	if(gameObject.transform.position.x < 0.25 || gameObject.transform.position.x > 0.8)
+	{
+		// show the Cow dead object 
+		gameObject.guiTexture.texture = Chick_dead;
+		// set the die flag
+		Chick_die = true;
 	}
 }
--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
@@ -2,6 +2,9 @@
 private var Cow_right : Texture;
 private var Cow_left : Texture;
 private var Cow_down : Texture;
+private var Cow_dead : Texture;
+private var Cow_r_dead : Texture;
+private var Cow_die = false;
 
 function Start () 
 {
@@ -9,26 +12,44 @@ function Start ()
 	Cow_right = Resources.Load("cow_right") as Texture;
     Cow_left = Resources.Load("cow_left") as Texture;
     Cow_down = Resources.Load("cow_down") as Texture;
+    Cow_dead = Resources.Load("cow_dead") as Texture;
+    Cow_r_dead = Resources.Load("cow_right_dead") as Texture;
 }
 
 function Update () 
 {
 	// respond to the left arrow
-	if(Input.GetKey(KeyCode.LeftArrow))
+	if(Input.GetKey(KeyCode.LeftArrow) && Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_left;
-		transform.Translate(-0.2 * Time.deltaTime, 0, 0);
+		transform.Translate(-0.3 * Time.deltaTime, 0, 0);
 	}
 	// respond to the right arrow
-	else if(Input.GetKey(KeyCode.RightArrow))
+	else if(Input.GetKey(KeyCode.RightArrow) && Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_right;
-		transform.Translate(0.2 * Time.deltaTime, 0, 0);
+		transform.Translate(0.3 * Time.deltaTime, 0, 0);
 	}
 	// default down picture
-	else
+	else if (Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_down;
 	}
-    
+	
+	// determine the border of the object's movement
+	// the middle x position is: x = 0.5
+	if(gameObject.transform.position.x < 0.25)
+	{
+		// show the Cow dead object 
+		gameObject.guiTexture.texture = Cow_dead;
+		// set the die flag
+		Cow_die = true;
+	}
+	else if(gameObject.transform.position.x > 0.8)
+	{
+		// show the Cow dead right object 
+		gameObject.guiTexture.texture = Cow_r_dead;
+		// set the die flag
+		Cow_die = true;
+	} 
 }
