9db8b43673fcf4ea9d8fd270491f80ead241cbb6
xiaojie3161 <xiaojie3161@hotmail.com>
2013-09-28 16:22:05 +0800

    Update chick and cow landing path
    Remove the Gravity properties for the chick_pre and cow_pre
    Remove constant force.
    update Chick.js and CowJS

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Chick.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Chick.js
@@ -5,8 +5,22 @@ private var Chick_down : Texture;
 private var Chick_dead : Texture;
 private var Chick_die = false;
 
+private var startPos:Vector3;
+private var k:float;
 function Start()
 {
+	// initialize variables
+	// initialize the velocity and start position
+ 	gameObject.rigidbody.velocity = new Vector3(0, -0.25,0);
+	startPos.x = 0.5;
+	startPos.y = 0.87;
+	startPos.z = 0;
+	// give random initilial direction (left or right)
+	var randomValue:float = Random.value;
+	if(randomValue<0.5)
+		k = startPos.y*8-(3.1415926/2);
+	else
+		k = startPos.y*8+(3.1415926/2);
 	// Initialize the resource picture
 	Chick_right = Resources.Load("chick_right") as Texture;
     Chick_left = Resources.Load("chick_left") as Texture;
@@ -15,22 +29,32 @@ function Start()
 }
 
 function Update () {
+	var targetPos: Vector3 = transform.position;
+	
 	// respond to the left arrow
 	if(Input.GetKey(KeyCode.LeftArrow) && Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_left;
 		transform.Translate(-0.3 * Time.deltaTime, 0, 0);
+		startPos.x += -0.3 * Time.deltaTime;
 	}
 	// respond to the right arrow
 	else if(Input.GetKey(KeyCode.RightArrow) && Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_right;
 		transform.Translate(0.3 * Time.deltaTime, 0, 0);
+		startPos.x += 0.3 * Time.deltaTime;
 	}
 	// default down picture
 	else if(Chick_die == false)
 	{
 		gameObject.guiTexture.texture = Chick_down;
+	}
+	
+	// Reset the position according to interaction
+	if (Chick_die == false){
+		targetPos.x = startPos.x+(1/(1+0.25*Time.time))* Mathf.Cos(targetPos.y*8-k);
+		transform.position = Vector3.Lerp (transform.position, targetPos, Time.deltaTime * 1.0f);
 	}
 	
 	// determine the border of the object's movement
--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/Cow.js
@@ -6,9 +6,24 @@ private var Cow_dead : Texture;
 private var Cow_r_dead : Texture;
 private var Cow_die = false;
 
+private var startPos:Vector3;
+private var k:float;
 function Start () 
 {
-	// initialize picture
+	// initialize variables
+	// initialize the velocity and start position
+ 	gameObject.rigidbody.velocity = new Vector3(0, -0.25,0);
+	startPos.x = 0.5;
+	startPos.y = 0.87;
+	startPos.z = 0;
+
+	// give random initilial direction (left or right)
+	var randomValue:float = Random.value;
+	if(randomValue<0.5)
+		k = startPos.y*10-(3.1415926/2);
+	else
+		k = startPos.y*10+(3.1415926/2);
+	
 	Cow_right = Resources.Load("cow_right") as Texture;
     Cow_left = Resources.Load("cow_left") as Texture;
     Cow_down = Resources.Load("cow_down") as Texture;
@@ -18,22 +33,32 @@ function Start ()
 
 function Update () 
 {
+	var targetPos: Vector3 = transform.position;
+	
 	// respond to the left arrow
 	if(Input.GetKey(KeyCode.LeftArrow) && Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_left;
 		transform.Translate(-0.3 * Time.deltaTime, 0, 0);
+		startPos.x+=-0.3 * Time.deltaTime;
+
 	}
 	// respond to the right arrow
 	else if(Input.GetKey(KeyCode.RightArrow) && Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_right;
 		transform.Translate(0.3 * Time.deltaTime, 0, 0);
+		startPos.x += 0.3 * Time.deltaTime;
 	}
 	// default down picture
 	else if (Cow_die == false)
 	{
 		gameObject.guiTexture.texture = Cow_down;
+	}
+	// Reset the position according to interaction
+	if (Cow_die == false){
+		targetPos.x = startPos.x+(1/(1.2+0.25*Time.time))* Mathf.Cos(targetPos.y*10-k);
+		transform.position = Vector3.Lerp (transform.position, targetPos, Time.deltaTime * 1.0f);
 	}
 	
 	// determine the border of the object's movement
