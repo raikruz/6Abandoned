9a59029e5ec1c040ea992169a2113a838e591a37
FrancisLuo2891 <luo_xiawu@163.com>
2013-10-03 00:59:32 +0800

    Add 'Picture' to the button of the Game Over picture.
    Need to Refine.

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/GameOver.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/GameOver.js
@@ -1,4 +1,6 @@
 #pragma strict
+public var playbtn : Texture2D = null;
+public var exitbtn : Texture2D = null;
 
 function Start () {
 
@@ -12,7 +14,7 @@ function OnGUI() {
 	if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.3 , 120, 50), "Play Again")) {
 		Application.LoadLevel(1);
 	}
-	else if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.3 + 70, 120, 50), "Quit")) {
+	else if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.3 + 70, 120, 50), exitbtn)) {
 		Application.LoadLevel(1);
 	}
 }
