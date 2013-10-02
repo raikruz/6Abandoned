aa08c848f48d4bfa92520b1fc380d9cafe3541df
FrancisLuo2891 <luo_xiawu@163.com>
2013-10-03 00:52:46 +0800

    Add the 'Play' & 'Exit' pictures to the buttons.
    Need refine.

--- AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/MainMenu.js
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/MainMenu.js
@@ -1,5 +1,6 @@
 #pragma strict
-
+public var playbtn : Texture2D = null;
+public var exitbtn : Texture2D = null;
 function Start () {
 
 }
@@ -9,10 +10,10 @@ function Update () {
 }
 
 function OnGUI() {
-	if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.5 , 120, 50), "Play")) {
+	if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.5 , 120, 50), playbtn)) {
 		Application.LoadLevel(1);
 	}
-	else if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.5 + 70, 120, 50), "Quit")) {
+	else if (GUI.Button(Rect(Screen.width / 2.2, Screen.height / 1.5 + 70, 120, 50), exitbtn)) {
 		Application.LoadLevel(1);
 	}
 }
