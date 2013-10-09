d1912b300fa861306129a96aba950bcfae187048
fatrat1117 <fatrat1117@gmail.com>
2013-10-09 13:11:56 +0800

    Score display

---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		GUIStyle style = new GUIStyle();
+		style.alignment = GUI.skin.label.alignment;
+		style.alignment = TextAnchor.MiddleRight;
+		style.normal.textColor = Color.yellow;
+		OTSprite sprite = GetComponent<OTSprite>();
+		GUI.Label (new Rect (0, 10, sprite.size.x - 8, 20), Main.animalSaved.ToString(), style);	
+	}
+}
