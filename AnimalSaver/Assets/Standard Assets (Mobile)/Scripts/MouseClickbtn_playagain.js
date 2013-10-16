74f1712873d8592884d8cf4bae0d2ccb19c06141
FrancisLuo2891 <luo_xiawu@163.com>
2013-10-17 00:07:59 +0800

    Add 'Play again' button in the Game Over Page.
    Need testing in tablet.

---
+++ AnimalSaver/Assets/Standard Assets (Mobile)/Scripts/MouseClickbtn_playagain.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var normalTxt : Texture2D;
+var hoverTxt : Texture2D;
+
+// mouse enter function
+function OnMouseEnter()
+{
+	guiTexture.texture = hoverTxt;
+}
+// mouse hover exit function
+function OnMouseExit()
+{
+	guiTexture.texture = normalTxt;
+}
+
+// mouse down function
+function OnMouseDown()
+{
+	// play sound
+	audio.Play();
+	// load application 1
+	Application.LoadLevel(1);
+}
